00c98ae27a4f880e72398de2f2db04e09a2446f6
zaid-kh <z.kh2800@gmail.com>
2023-11-07 10:00:58 +0200

    update

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,8 +3,11 @@ const table = document.querySelector("table");
 for (let i = 1; i <= 10; i++) {
   output += "<tr>";
   for (let j = 1; j <= 20; j++) {
-    if (i === 1 || j === 1) output += "<th>" + i * j + "</th>";
-    else output += "<td>" + i * j + "</td>";
+    if (i === 1 && j === 1) output = "<th></th>";
+    else {
+      if (i === 1 || j === 1) output += "<th>" + i * j + "</th>";
+      else output += "<td>" + i * j + "</td>";
+    }
   }
   output += "</tr>";
 }
